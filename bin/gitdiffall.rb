0937c77c3668ef3b5ce297c5c4f26c82d8fc87e5
bootleq <bootleq@gmail.com>
2011-09-29 22:42:11 +0800

    Add some git-diff options to ruby script.

--- bin/gitdiffall.rb
+++ bin/gitdiffall.rb
@@ -7,17 +7,39 @@ MAX_FILES = 14
 MIN_HASH_ABBR = 5
 
 opt =  OptionParser.new
-opt.banner = "Usage: gitdiffall [range] [options]"
-# TODO take arbitrary git-diff options
-# opt.on('-c', '--cached', 'Use git-diff with --cached option') {|v| }
-opt.parse!(ARGV)
+opt.banner = "Usage: gitdiffall [revision] [diff-options] [--] [<path>...]"
 
-# Range example:
+revision, diff_opts, pathes = '', [], ''
+
+opt.on('-c', '--cached', '(delegate to git-diff)') {|v| diff_opts << "-c"}
+opt.on('--no-rename', '(delegate to git-diff)') {|v| diff_opts << "--no-renames"}
+opt.on('-B[<n>][/<m>]', '--break-rewrites[=[<n>][/<m>]]', '(delegate to git-diff)') {|v| diff_opts << "-B#{v}"}
+opt.on('-M[<n>]', '--find-renames[=[<n>]]', '(delegate to git-diff)') {|v| diff_opts << "-M#{v}"}
+opt.on('-C[<n>]', '--find-copies[=[<n>]]', '(delegate to git-diff)') {|v| diff_opts << "-C#{v}"}
+opt.on('--find-copies-harder', '(delegate to git-diff)') {|v| diff_opts << "--find-copies-harder"}
+opt.on('-D', '--irreversible-delete', '(delegate to git-diff)') {|v| diff_opts << "-D"}
+opt.on('-l num', '(delegate to git-diff)') {|v| diff_opts << "-l #{v}"}
+opt.on('--diff-filter=filter', '(delegate to git-diff)') {|v| diff_opts << "--diff-filter=#{v}"}
+opt.on('-S[string]', '(delegate to git-diff)') {|v| diff_opts << "-S#{v}"}
+opt.on('-G[regex]', '(delegate to git-diff)') {|v| diff_opts << "-G#{v}"}
+opt.on('--pickaxe', '(delegate to git-diff)') {|v| diff_opts << "--pickaxe"}
+opt.on('-O[orderfile]', '(delegate to git-diff)') {|v| diff_opts << "-O#{v}"}
+opt.on('-R', '(delegate to git-diff)') {|v| diff_opts << "-R"}
+opt.on('--relative[=path]', '(delegate to git-diff)') {|v| diff_opts << "--relative=#{v}"}
+opt.on('-a', '--text', '(delegate to git-diff)') {|v| diff_opts << "-a"}
+opt.on('-b', '--ignore-space-change', '(delegate to git-diff)') {|v| diff_opts << "-b"}
+opt.on('-w', '--ignore-all-space', '(delegate to git-diff)') {|v| diff_opts << "-w"}
+opt.on('--ignore-submodules[=<when>', '(delegate to git-diff)') {|v| diff_opts << "--ignore-submodules"}
+
+pathes = ARGV.slice!(ARGV.index('--'), ARGV.length).join(' ') if ARGV.index('--')
+opt.parse!(ARGV)
+revision = ARGV.join(' ')
+
+# revision example:
 #   (nil)       - see current (unstaged) changes
 #   <commit>    - see current changes, compare with <commit>
 #   @<commit>   - compare <commit> with it's previous commit (liner shown in git log)
 #   4 (number)  - shortcut for @<commit> where commit is the <number>-th previous one
-range = opt.default_argv.first
 
 if %x(git rev-parse --is-inside-work-tree) == 'false'
   puts 'Not inside a git working tree.'
@@ -26,18 +48,18 @@ elsif $?.exitstatus != 0
   abort
 end
 
-if String(range).match(/^@\w/)
-  range = %x(git log --format=format:"_" #{range[1..-1]}..).lines.to_a.length.to_s
-  puts "Shortcut for this commit is #{range}.\n\n"
+if String(revision).match(/^@\w/)
+  revision = %x(git log --format=format:"_" #{revision[1..-1]}..).lines.to_a.length.to_s
+  puts "Shortcut for this commit is #{revision}.\n\n"
 end
 
-if range.to_i.to_s == range and range.length < MIN_HASH_ABBR
-  rev = %x(git log -1 --skip=#{range} --format=format:"%h")
-  previous = %x(git log -1 --skip=#{range.to_i + 1} --format=format:"%h")
-  range = "#{rev}..#{previous}"
+if revision.to_i.to_s == revision and revision.length < MIN_HASH_ABBR
+  rev = %x(git log -1 --skip=#{revision} --format=format:"%h")
+  previous = %x(git log -1 --skip=#{revision.to_i + 1} --format=format:"%h")
+  revision = "#{rev}..#{previous}"
 end
 
-files = %x{git diff --name-only --relative #{range}}.chomp
+files = %x{git diff --name-only --relative #{revision}}.chomp
 count = files.lines.to_a.length
 
 if count > MAX_FILES
@@ -50,7 +72,7 @@ if count > MAX_FILES
 end
 
 if count > 0
-  system("vim -p #{files.gsub(/\n/, ' ')} -c 'tabdo GitDiff #{range}' -c 'tabfirst'")
+  system("vim -p #{files.gsub(/\n/, ' ')} -c 'tabdo GitDiff #{revision}' -c 'tabfirst'")
 else
   puts '# Changes outside this directory are ignored' if %x(git rev-parse --show-prefix) != ''
   puts 'no differences'
